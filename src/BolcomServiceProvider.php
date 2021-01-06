bf0fcf3db5a7ed8044a14ba9819b9918718a3f10
Mark van den Broek <mvdnbrk@gmail.com>
2021-01-06 16:03:05 +0100

    Extract to `registerPublishing` method

--- src/BolcomServiceProvider.php
+++ src/BolcomServiceProvider.php
@@ -8,15 +8,20 @@ class BolcomServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/bol.php' => config_path('bol.php'),
-            ], 'bolcom-config');
-        }
+        $this->registerPublishing();
     }
 
     public function register(): void
     {
         $this->mergeConfigFrom(__DIR__.'/../config/bol.php', 'bol');
+    }
+
+    private function registerPublishing(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/bol.php' => config_path('bol.php'),
+            ], 'bolcom-config');
+        }
     }
 }
